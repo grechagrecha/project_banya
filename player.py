39f331bcfc827ea950fdcddb1a39641d1358b755
ruspo <ruspolak69@gmail.com>
2022-10-13 15:54:37 +0300

    some refactoring

--- player.py
+++ player.py
@@ -1,8 +1,6 @@
 import pygame
 
-
 from pygame.math import Vector2
-
 
 from constants import PLAYER_SPEED
 
@@ -12,18 +10,35 @@ class Movement:
         super().__init__()
         self.direction = Vector2()
         self.stopped = False
+        self.max_speed = PLAYER_SPEED
+        self.speed = 0
+        self.acceleration = 4
 
     def __configure_direction(self, x, y):
         self.direction.update(x, y)
+
         if self.direction.magnitude() != 0:
             self.direction.normalize_ip()
+        if self.is_stopped():
+            pass
 
-    def set_direction(self, x, y):
+    def set_direction(self, x=0, y=0):
         if not (x or y):
             self.stopped = True
         else:
             self.__configure_direction(x, y)
             self.stopped = False
+
+    def __calculate_speed(self):
+        if self.is_stopped():
+            if self.speed > 0:
+                self.speed -= self.acceleration
+        elif self.speed <= self.max_speed - self.acceleration:
+            self.speed += self.acceleration
+
+    def get_pos_shift(self, dt):
+        self.__calculate_speed()
+        return self.direction * self.speed * dt
 
     def is_stopped(self):
         return self.stopped
@@ -40,18 +55,9 @@ class Player(pygame.sprite.Sprite):
 
         self.movement = Movement()
         self.pos = Vector2(*start_pos)
-        self.max_speed = PLAYER_SPEED
-        self.speed = 0
-        self.acceleration = 4
 
     def move(self, dt):
-        if self.movement.is_stopped():
-            if self.speed > 0:
-                self.speed -= self.acceleration
-        elif self.speed <= self.max_speed:
-            self.speed += self.acceleration
-
-        self.pos += self.movement.direction * self.speed * dt
+        self.pos += self.movement.get_pos_shift(dt)
         self.rect.center = self.pos
 
     def update(self, dt):
