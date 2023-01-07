9cc2ed43fd623d51c9e210af611b05aee30d902c
grecha_grecha <ruspolak69@gmail.com>
2023-01-07 03:11:41 +0300

    fixed again

--- event_handler.py
+++ event_handler.py
@@ -1,24 +1,36 @@
 import pygame
 import sys
 
+from player import Player
 
-def handle_events(player):
-    handle_keys(player)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+class EventHandler:
+    def __init__(self, player: Player):
+        self._keys = pygame.key.get_pressed()
+        self._events = pygame.event.get()
+        self._player = player
+
+    def _handle_events(self):
+        for event in self._events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def _handle_keys(self):
+        if self._keys[pygame.K_ESCAPE]:
             pygame.quit()
             sys.exit()
 
+        self._player.movement.set_direction(
+            x=self._keys[pygame.K_d] - self._keys[pygame.K_a],
+            y=self._keys[pygame.K_s] - self._keys[pygame.K_w]
+        )
 
-def handle_keys(player):
-    keys = pygame.key.get_pressed()
+    def _update(self):
+        self._keys = pygame.key.get_pressed()
+        self._events = pygame.event.get()
 
-    if keys[pygame.K_ESCAPE]:
-        pygame.quit()
-        sys.exit()
-
-    player.movement.set_direction(
-        x=keys[pygame.K_d] - keys[pygame.K_a],
-        y=keys[pygame.K_s] - keys[pygame.K_w]
-    )
+    def run(self):
+        self._update()
+        self._handle_events()
+        self._handle_keys()
--- game.py
+++ game.py
@@ -2,13 +2,13 @@ import pygame
 
 from settings.settings_management import get_resolution, get_framerate
 from player import Player
-from event_handler import handle_events
+from event_handler import EventHandler
 from debug import Debug
 
 
 class Game:
     def __init__(self, *, debug_mode=False):
-        pygame.init()
+        pygame.init() if not pygame.init() else ...
 
         self._resolution = get_resolution()
         self._framerate = get_framerate()
@@ -16,6 +16,7 @@ class Game:
         self.player_sprite = pygame.sprite.GroupSingle()
 
         self.player = Player((320, 160), self.player_sprite)
+        self.event_handler = EventHandler(self.player)
 
         self.screen = pygame.display.set_mode(self._resolution)
         pygame.display.set_caption('project banya')
@@ -26,32 +27,35 @@ class Game:
         self.debug = Debug() if debug_mode else None
 
     def update(self):
-
         self.player_sprite.update(self.dt)
-
         pygame.display.update()
 
         self.dt = self.clock.tick(self._framerate) / 1000
 
     def draw(self):
+        """ Main draw function """
+
         self.screen.fill('grey')
 
         self.player_sprite.draw(self.screen)
 
         if self.debug:
             self.debug.show_debug(
-                self.player.movement.direction,
-                self.player.pos,
-                self.player.movement.speed
+                f'dir: {self.player.movement.direction} |',
+                f'pos: {self.player.pos.x:.1f} {self.player.pos.y:.1f} |',
+                f'vel: {self.player.movement.velocity.x:.1f} {self.player.movement.velocity.y:.1f}|',
+                f'is_mov: {self.player.movement.moving} |'
+                f'vel_mag: {self.player.movement.velocity.magnitude():.3f}'
             )
 
     def run(self):
+        """ Main game loop """
 
         # Initial dt calculation
         self.dt = self.clock.tick(self._framerate) / 1000
 
         while True:
-            handle_events(self.player)
+            self.event_handler.run()
 
             self.update()
             self.draw()
